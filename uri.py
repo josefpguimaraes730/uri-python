083dc01391090d7cac9b1397ed8e26af96a846e3
josefpguimaraes <jose.guimaraes@dock.tech>
2023-10-18 18:39:01 -0300

    Base code

--- uri.py
+++ uri.py
@@ -1,1 +1,18 @@
+#python -i uri.py < in.txt > out.txt
+
+#solution algorithm
+def solve(line):
+  print(line)
+  return
+
+#remove \n came from stdin
+def formatLine(line):
+    linebreak = line.find('\n')
+    return line[:linebreak] if linebreak > 0 else line
+
+#main function gets the input
+import sys
+
+for line in sys.stdin:
+    solve(formatLine(line))
 
